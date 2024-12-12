6d76ffe01d98c85a98d5545a92cac3a990210dd1
futurGH <me@futuristick.ca>
2024-12-12 13:00:24 -0500

    Use CTE for like updateAggregates

--- packages/bsky/src/data-plane/server/db/util.ts
+++ packages/bsky/src/data-plane/server/db/util.ts
@@ -1,4 +1,5 @@
 import {
+  AliasedRawBuilder,
   DummyDriver,
   DynamicModule,
   ExpressionBuilder,
@@ -40,6 +41,38 @@ export const noMatch = sql`1 = 0`
 // Can be useful for large where-in clauses, to get the db to use a hash lookup on the list
 export const valuesList = (vals: unknown[]) => {
   return sql`(values (${sql.join(vals, sql`), (`)}))`
+}
+
+// More advanced version allowing aliasing
+// reference: https://kysely.dev/docs/recipes/extending-kysely#a-more-complex-example
+export const valuesAliased = <
+  R extends Record<string, unknown>,
+  A extends string,
+>(
+  records: R[],
+  alias: A,
+): AliasedRawBuilder<R, A> => {
+  // Assume there's at least one record and all records
+  // have the same keys.
+  const keys = Object.keys(records[0])
+
+  // Transform the records into a list of lists such as
+  // ($1, $2, $3), ($4, $5, $6)
+  const values = sql.join(
+    records.map((r) => sql`(${sql.join(keys.map((k) => r[k]))})`),
+  )
+
+  // Create the alias `v(id, v1, v2)` that specifies the table alias
+  // AND a name for each column.
+  const wrappedAlias = sql.ref(alias)
+  const wrappedColumns = sql.join(keys.map(sql.ref))
+  const aliasSql = sql`${wrappedAlias}(${wrappedColumns})`
+
+  // Finally create a single `AliasedRawBuilder` instance of the
+  // whole thing. Note that we need to explicitly specify
+  // the alias type using `.as<A>` because we are using a
+  // raw sql snippet as the alias.
+  return sql<R>`(values ${values})`.as<A>(aliasSql)
 }
 
 export const dummyDialect = {
